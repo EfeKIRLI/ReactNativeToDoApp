35bf5b1f49093c0c5e390566691c2de414e6d198
EFE SAMED KIRLI <efesamed@outlook.com.tr>
2024-05-04 19:10:30 +0300

    home screen

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,17 +1,49 @@
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import style from './style'
+import { FlatList } from 'react-native-gesture-handler'
 
+const initialTasks = [
+    {
+        title: 'Save the Tasks',
+        isCompleted: true
+    },
+    {
+        title: 'Save the Tasks By writing ',
+        isCompleted: false
+    },
+    {
+        title: 'add a new task ',
+        isCompleted: false
+    },
+    {
+        title: 'change a new task ',
+        isCompleted: false
+    },
+    {
+        title: 'sperate in 3 tabs as: List - Add - Settings ',
+        isCompleted: false
+    },
+
+]
 
 const Home = () => {
+
+    const [tasks, setTasks] = useState(initialTasks)
     return (
 
-        <View>
+        <View style={style.container}>
+            <Text>My To Do Screens </Text>
+
+            <FlatList data={tasks} 
+            renderItem={(item) => <Text>{item} </Text>}
             
-                <View>
-                    <Text>Home</Text>
-                </View>
+            /> 
+
+        
         </View>
-    
+
+
     )
 }
 
